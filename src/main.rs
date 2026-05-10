15f7c64ff0401fff710092e9204b30058a3a0559
Lucas Pickering <lucas@lucaspickering.me>
2026-05-10 18:23:16 -0400

    Xilem initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-//! A TUI program to be displayed on a Raspberry Pi touchscreen. This uses an
-//! ELM-like architecture:
-//! - The main thread monitors a message mpsc queue
-//! - Background threads are spawned to monitor external state
-//! - To modify state, the background threads send messages to the main thread
-//! - Whenever state is changed, redraw the terminal
+//! A GUI program to be displayed on a Raspberry Pi touchscreen
 //!
 //! This is a panic-first type program. Most errors are fatal. Anyhow has no
 //! power here!!
@@ -15,85 +10,28 @@ mod util;
 mod view;
 mod weather;
 
-use crate::{
-    config::Config,
-    state::{Message, State, Tx},
-    util::spawn,
-    view::DIMENSIONS,
-};
-use ratatui::{
-    DefaultTerminal, Terminal, TerminalOptions, Viewport,
-    crossterm::{
-        self,
-        event::{
-            self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode,
-            KeyEvent, MouseButton, MouseEvent, MouseEventKind,
-        },
-        terminal::{EnterAlternateScreen, LeaveAlternateScreen},
-    },
-    prelude::CrosstermBackend,
-};
-use std::{
-    fs::OpenOptions,
-    io::{self, Stdout},
-    sync::mpsc,
-};
-use tracing::{info, level_filters::LevelFilter};
+use crate::{config::Config, state::State};
+use std::fs::OpenOptions;
+use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{
     Layer, filter::Targets, fmt::format::FmtSpan, layer::SubscriberExt,
     util::SubscriberInitExt,
 };
+use xilem::{EventLoop, WindowOptions, Xilem, winit::error::EventLoopError};
 
-/// Initialize the TUI and start the main loop
-fn main() {
-    initialize_tracing();
+fn main() -> Result<(), EventLoopError> {
+    // TODO fix verbose logging
+    //     initialize_tracing();
     let config = Config::load();
-
-    let terminal = initialize_terminal();
-    run(config, terminal);
-    restore_terminal();
-}
-
-/// Start the main program loop
-fn run(config: Config, mut terminal: DefaultTerminal) {
-    let mut state = State::default();
-
-    let (tx, rx) = mpsc::channel();
-    let tx = Tx::new(tx);
-
-    // Spawn background tasks
-    spawn(&config, &tx, move |_, tx| {
-        // Listen for signals. The termination feature is enabled so this
-        // catches SIGTERM and SIGHUP as well
-        ctrlc::set_handler(move || tx.send(Message::Quit)).unwrap();
-    });
-    spawn(&config, &tx, move |_, tx| {
-        // Input handler
-        loop {
-            match event::read() {
-                Ok(event) => {
-                    if let Some(message) = input_message(event) {
-                        tx.send(message);
-                    }
-                }
-                // Input closed - exit
-                Err(_) => tx.send(Message::Quit),
-            }
-        }
-    });
-    spawn(&config, &tx, transit::transit_loop);
-    spawn(&config, &tx, weather::weather_loop);
-
-    loop {
-        terminal.draw(|frame| view::draw(frame, &state)).unwrap();
-        // Block until we get a message
-        match rx.recv().unwrap() {
-            Message::NextMode => state.mode = state.mode.next(),
-            Message::Quit => break,
-            Message::Transit(transit) => state.transit = transit,
-            Message::Weather(weather) => state.weather = weather,
-        }
-    }
+    // spawn(&config, &tx, transit::transit_loop);
+    // spawn(&config, &tx, weather::weather_loop);
+    let app = Xilem::new_simple(
+        State::default(),
+        view::app_logic,
+        WindowOptions::new("Heisenberg"),
+    );
+    app.run_in(EventLoop::with_user_event())?;
+    Ok(())
 }
 
 fn initialize_tracing() {
@@ -121,56 +59,4 @@ fn initialize_tracing() {
         .with_span_events(FmtSpan::NONE)
         .with_filter(targets);
     tracing_subscriber::registry().with(file_subscriber).init()
-}
-
-fn initialize_terminal() -> Terminal<CrosstermBackend<Stdout>> {
-    info!("Initializing terminal");
-    // Restore terminal on exit
-    let original_hook = std::panic::take_hook();
-    std::panic::set_hook(Box::new(move |panic_info| {
-        restore_terminal();
-        original_hook(panic_info);
-    }));
-
-    crossterm::terminal::enable_raw_mode().unwrap();
-    crossterm::execute!(
-        io::stdout(),
-        EnterAlternateScreen,
-        EnableMouseCapture,
-    ).unwrap();
-    Terminal::with_options(
-        CrosstermBackend::new(io::stdout()),
-        TerminalOptions {
-            // Lock the terminal to the Pi's dimensions
-            viewport: Viewport::Fixed(DIMENSIONS.into()),
-        },
-    )
-    .unwrap()
-}
-
-/// Set the terminal like we found it
-fn restore_terminal() {
-    info!("Restoring terminal");
-    let _ = crossterm::terminal::disable_raw_mode();
-    let _ = crossterm::execute!(
-        io::stdout(),
-        LeaveAlternateScreen,
-        DisableMouseCapture,
-    );
-}
-
-/// Handle user input and build the corresponding message. Return `None` if
-/// the event should be ignored
-fn input_message(event: Event) -> Option<Message> {
-    match event {
-        Event::Key(KeyEvent {
-            code: KeyCode::Esc, ..
-        }) => Some(Message::Quit),
-        // Cycle mode on click/tap
-        Event::Mouse(MouseEvent {
-            kind: MouseEventKind::Up(MouseButton::Left),
-            ..
-        }) => Some(Message::NextMode),
-        _ => None,
-    }
 }
